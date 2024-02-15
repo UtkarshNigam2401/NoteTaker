ec7fbb07290a16563a22a27935e53bbb848ca32e
Utkarsh Nigam <74413725+UtkarshNigam2401@users.noreply.github.com>
2024-02-15 20:31:49 +0530

    Updated try catch statements for better exception handling.

--- src/main/java/com/servlet/SaveNoteServlet.java
+++ src/main/java/com/servlet/SaveNoteServlet.java
@@ -5,10 +5,10 @@ import java.io.PrintWriter;
 import java.util.Date;
 
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -29,19 +29,8 @@ public class SaveNoteServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-//
-//	/**
-//	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-//	 */
-//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		// TODO Auto-generated method stub
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
-//	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		try {
 			//title, Content fetch
 				String title = request.getParameter("title");
@@ -61,7 +50,10 @@ public class SaveNoteServlet extends HttpServlet {
 				out.println("<h1 style='text-align: center;'><a href='all_notes.jsp'>View all notes<a/><h1/>"); 				
 				
 		}catch (Exception e) {
-			// TODO: handle exception
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+			out.println("<h1 style = 'text-align: center; color: red;'> Failed to add note. Please try again.</h1>");
+			e.printStackTrace(out);
 			e.printStackTrace();
 		}
 	}
